ee6cc4fc5ffbf1f17a84ec813409b2335c20a3df
khj011219 <khj011219@naver.com>
2026-05-21 15:34:05 +0900

    refactor: PostService UserRepository → UserService로 교체 및 Lombok 적용

---
+++ src/main/java/org/sopt/controller/UserController.java
@@ -0,0 +1,27 @@
+package org.sopt.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.sopt.dto.request.CreateUserRequest;
+import org.sopt.dto.response.BaseResponse;
+import org.sopt.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "User", description = "유저 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public ResponseEntity<BaseResponse<Void>> join(
+            @RequestBody CreateUserRequest request
+    ) {
+        userService.join(request.nickname(), request.email(), request.paasword());
+        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.success(null));
+    }
+}
---
+++ src/main/java/org/sopt/service/UserService.java
@@ -0,0 +1,27 @@
+package org.sopt.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.domain.User;
+import org.sopt.exception.ErrorCode;
+import org.sopt.exception.NotFoundException;
+import org.sopt.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void join(String nickname, String email, String paasword) {
+        User user = new User(nickname, email, paasword);
+        userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+    }
+}
