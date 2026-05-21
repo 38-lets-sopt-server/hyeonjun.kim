263fa2750ba6211e81d9e9500f1bc3abd81b43c6
khj011219 <khj011219@naver.com>
2026-05-21 15:59:30 +0900

    fix: SecurityConfig 회원가입 경로 permitAll 추가

--- src/main/java/org/sopt/config/SecurityConfig.java
+++ src/main/java/org/sopt/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/login", "/api/v1/auth/reissue").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/posts/**", "/posts").permitAll()
+                        .requestMatchers("/users").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
